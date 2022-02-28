6ba144203c7d21310968e569faf2a4be4a8b0dfb
Efim <evp145@gmail.com>
2022-02-28 22:36:30 +0300

    добавлено последнее задание

--- dz6/5.py
+++ dz6/5.py
@@ -1,6 +1,20 @@
 class Stationery:
-    def __init__(self, title):
-        self.title = title
-
+    title = "title"
     def draw(self):
-        return f'Запуск отрисовки'
+        print('Запуск отрисовки.')
+class Pen(Stationery):
+    def draw(self):
+        print('Запуск отрисовки ручкой')
+class Pencil(Stationery):
+    def draw(self):
+        print('Запуск отрисовки карандашом')
+class Handle(Stationery):
+    def draw(self):
+        print('Запуск отрисовки маркером')
+
+m_pen = Pen()
+m_pencil = Pencil()
+m_handle = Handle()
+m_pen.draw()
+m_pencil.draw()
+m_handle.draw()
