f8162380f26956d28089d3e098f52cb362bfbcbb
blaxcloud <evp14@mail.ru>
2022-03-07 15:14:35 +0300

    Последняя отладка

--- dz7/2.py
+++ dz7/2.py
@@ -1,32 +1,28 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Clothes(ABC):
+class ABSClothes(ABC):
+    @abstractmethod
+    def rasch(self):
+        pass
+
+class Clothes(ABSClothes):
 
     def __init__(self, param1, param2):
         self.param1 = param1
         self.param2 = param2
 
-
-    def rasch(self):
-        pass
-
-
-
-
     @property
-    def get_sq_full(self):
+    def rasch(self):
         return str(f'Общая площадь ткани  {(self.param1 / 6.5 + 0.5) + (self.param2 * 2 + 0.3)}')
 
 
 class Coat(Clothes):
-
 
     @property
     def rasch(self):
         return self.param1 / 6.5 + 0.5
 
 class suit(Clothes):
-
 
     @property
     def rasch(self):
@@ -37,9 +33,9 @@ Suit_r = suit(52, 4)
 Coat_r = Coat(52, 4)
 print(suit)
 print(Coat)
-print(Clothes.get_sq_full)
+print(Clothes.rasch)
 print(f'Площадь ткани на костюм {Suit_r.rasch}')
 print(f'Площадь ткани на пальто {Coat_r.rasch}')
-print(clothes_r.get_sq_full)
+print(clothes_r.rasch)
 
 
