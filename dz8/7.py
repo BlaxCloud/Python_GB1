1a4dd8399c7c0638c07075f66e7d9132c85b9eac
Efim <evp145@gmail.com>
2022-03-12 19:18:17 +0300

    финальные правки1

--- dz8/7.py
+++ dz8/7.py
@@ -1,23 +1,24 @@
 class ComplexNumber:
-    def __init__(self, a, b, *args):
-        self.a = a
-        self.b = b
+    def __init__(self, re, im, *args):
+        self.re = re
+        self.im = im
         self.z = 'a + b * i'
 
-    def __add__(self, other):
-        print(f'Сумма z1 и z2 равна')
-        return f'z = {self.a + other.a} + {self.b + other.b} * i'
 
     def __mul__(self, other):
         print(f'Произведение z1 и z2 равно')
-        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+        return f'z = {self.re * other.re - (self.im * other.im)} + {self.im * other.re} * i'
+
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.re + other.re} + {self.im + other.im} * i'
 
     def __str__(self):
-        return f'z = {self.a} + {self.b} * i'
+        return f'z = {self.re} + {self.im} * i'
 
 
-z_1 = ComplexNumber(1, -2)
-z_2 = ComplexNumber(3, 4)
+z_1 = ComplexNumber(4, 6)
+z_2 = ComplexNumber(5, 7)
 print(z_1)
 print(z_1 + z_2)
 print(z_1 * z_2)
