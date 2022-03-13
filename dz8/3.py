57d93b9c5f078d053fc8a63a4be27ba105212763
Efim <evp145@gmail.com>
2022-03-13 10:54:52 +0300

    финальные правки 2

--- dz8/3.py
+++ dz8/3.py
@@ -1,28 +1,30 @@
-class NumberTakeExeption:
+class NumberTakeException(Exception):# верная версия с исключением
     def __init__(self, number):
         self.number = number
 
     def __str__(self):
         return str(f'{self.number} не являеются числом')
 
-class NumberTake:
-    NewList: list
 
-    def __init__(self):
-        self.NewList = []
 
-    def operation(self):
-        while True:
-            ch = input('Осуществите ввод>>> ')
-            if ch.isdigit():
-                self.NewList.append(ch)
-                print(f'Список на данный момент:{self.NewList}')
-            else:
-                if ch == 'Q':
-                    print('Exit')
-                    exit()
-                else:
-                    print(NumberTakeExeption(ch))
 
-try_ex = NumberTake()
-print(try_ex.operation())
+NewList = []
+while True:
+   ch = input('Осуществите ввод>>> ')
+   if ch == 'Q':
+       print('Exit')
+
+       exit()
+   try:
+       if not ch.isdigit():
+           raise NumberTakeException(ch)
+       NewList.append(ch)
+       print(NewList)
+
+   except NumberTakeException:
+       print(NumberTakeException(ch))
+
+
+
+
+
